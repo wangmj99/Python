e2d1a7070c3014e89520839af6ba850b2392f278
wangmj99 <45906771+wangmj99@users.noreply.github.com>
2021-05-28 10:39:02 -0700

    Add files via upload

---
+++ standardplot.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0, 20, 100)  # Create a list of evenly-spaced numbers over the range
+#plt.plot(x, np.sin(x))       # Plot the sine ofpyth each x point
+
+#plt.show()                   # Display the plot
+
+data = {'a': np.arange(50),
+        'c': np.random.randint(0, 50, 50),
+        'd': np.random.randn(50)}
+data['b'] = data['a'] + 10 * np.random.randn(50)
+data['d'] = np.abs(data['d']) * 100
+
+plt.scatter('a', 'b', c='c', s='d', data=data)
+plt.xlabel('entry a')
+plt.ylabel('entry b')
+plt.show()
