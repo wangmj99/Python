505f53b2134a6eebc185f05fa523a21631dd8d17
MW <wangmj99@gmail.com>
2021-06-30 15:00:12 -0700

    MW

--- helloword.py
+++ helloword.py
@@ -1,4 +1,6 @@
 from scipy.optimize import minimize
+import pandas as pd
+import matplotlib.pyplot as plt
 
 def eqn(x):
   return x**2 + x + 2
@@ -7,3 +9,48 @@ mymin = minimize(eqn, 0, method='BFGS')
 
 print(mymin)
 print('test tets and test 2121')
+
+arr = [1,2,3,4,5,6,-2]
+
+arr2 = [x*3 for x in arr if x%2==0]
+print(arr2)
+
+mydataset = {
+  'cars': ["BMW", "Volvo", "Ford"],
+  'passings': [3, 7, 2]
+}
+
+myvar = pd.DataFrame(mydataset)
+
+print(myvar)
+
+df = pd.read_csv('./data/data.csv')
+dfcorr = df.corr()
+print(type(dfcorr))
+print(dfcorr)
+#print(df['Mix'].mean())
+#print(df['Mix'].std())
+
+#print(df['Equity'].mean())
+#print(df['Equity'].std())
+df['Duration'].plot(kind = 'hist')
+##plt.show()
+
+nums = [i for i in range(1,1001)]
+string = "Practice Problems to Drill List Comprehension in Your Head."
+
+#List comprehension Quesitons
+#Find all of the numbers from 1–1000 that are divisible by 8
+q1 = [i for i in nums if i%8==0]
+
+#Find all of the numbers from 1–1000 that have a 6 in them
+q2 = [i for i in nums if '6' in str(i)] 
+
+#Count the number of spaces in a string
+q3 = len([c for c in string if c== ' '])
+
+#4. Remove all of the vowels in a string
+q4 = "".join([c for c in string if c not in ['a', 'e', 'o', 'i', 'u']])
+
+#7. Use a nested list comprehension to find all of the numbers from 1–1000 that are divisible by any single digit besides 1 (2–9)
+q7 = [i for i in nums if True in [True for divisor in range(2,10) if i%divisor == 0]]
---
+++ sp500_mov_avg.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import matplotlib.pyplot as plt 
+
+
+sp500 = pd.read_csv('./data/indice/SP500.csv', index_col=0)
+
+sp500['Price1'] =sp500['Close'].shift(-1)
+
+sp500['mvAvg20'] = sp500['Close'].rolling(20).mean()
+sp500['mvAvg50'] = sp500['Close'].rolling(50).mean()
+sp500['Hold'] = [1 if sp500['mvAvg20'][idx]>sp500['mvAvg50'][idx] else 0 for idx in sp500.index]
+sp500['PnL'] = [sp500['Price1'][idx] - sp500['Close'][idx] if sp500['Hold'][idx]>0 else 0 for idx in sp500.index ]
+sp500['TotalReturn'] = sp500['PnL'].cumsum()
+sp500['TotalReturnPct'] = sp500['TotalReturn']/sp500['Close'][0]-1
+sp500['SpyPnL'] = sp500['Price1'] - sp500['Close']
+sp500['SpyTotalReturn'] = sp500['SpyPnL'].cumsum()
+sp500['SpyTotalReturnPct'] = sp500['SpyTotalReturn']/sp500['Close'][0]-1
+sp500.dropna(inplace= True)
+
+print(sp500.tail())
+
+path_save = './data/sp500MVAvg.csv'
+sp500.to_csv(path_save)
+
+sp500['TotalReturn'].plot(color = 'green')
+sp500['SpyTotalReturn'].plot(color = 'red')
+plt.show()
